4bc0a9da163e483e5f4d2ac80085075f313afb34
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-02-12 13:56:55 +0200

    Create errors middleware and modify the app.js

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const products = require('./routes/products');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
 
 // middleware
 app.use(express.json());
 
-// routes
+// product routes
 app.use('/api/v1/products', products);
+
+// error middleware
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, console.log(`Server is running on port ${port}`));
+const start = async () => {
+  try {
+    // connect to DB
+    // server port
+    app.listen(port, console.log(`Server is running on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
